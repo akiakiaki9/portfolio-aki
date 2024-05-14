15ed3b1e71fd682bde589516dc4ecf09eedaf7c1
Akbar <tillayevakbarshoh@gmail.com>
2024-05-14 10:24:48 +0500

    fist

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,9 +2,145 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { IoMdInformationCircle } from "react-icons/io";
 
+//npm install react-intersection-observer react-spring (close)
+
 export default function Skills() {
+
+  const skills = [
+    {
+      id: 1,
+      title: 'SMM',
+      body: 'Social Media Marketing, or SMM, is the process of promoting a product or service through social media platforms such as Facebook, Instagram, Twitter, and LinkedIn.',
+      link: 'https://en.wikipedia.org/wiki/Social_media_marketing'
+    },
+    {
+      id: 2,
+      title: 'Frontend Development',
+      body: 'Frontend development is the process of creating the user interface (UI) for web applications or websites. It involves working with HTML, CSS, and JavaScript to build interactive and visually appealing user interfaces.',
+      link: 'https://en.wikipedia.org/wiki/Front-end_web_development'
+    },
+    {
+      id: 3,
+      title: 'Backend Development',
+      body: 'Backend development involves building and maintaining the server-side of web applications or websites. Unlike frontend development, which focuses on the user interface and client-side functionality.',
+      link: 'https://en.wikipedia.org/wiki/Frontend_and_backend'
+    },
+    {
+      id: 4,
+      title: 'Testing and Optimization',
+      body: 'Overall, testing and optimization are essential for delivering high-quality software that meets user expectations and performs well in various environments. By continuously testing and optimizing software throughout the development process.',
+      link: 'https://en.wikipedia.org/wiki/Web_testing'
+    }
+  ]
+
   return (
-    <div id="services" className="services">
+    <div className="services">
+      <div className="services-sar">
+        <h1>SKILLS
+          <div className="sar-animate"></div>
+        </h1>
+      </div>
+      <div className="services-blok">
+        {skills.map(skill => (
+          <Link key={skill.id} to='/skills' className="services-blok__section">
+            <div className="services-blok__icon">
+            </div>
+            <b>{skill.title}</b>
+            <p>{skill.body}</p>
+            <button><a href={skill.link}>Info <IoMdInformationCircle className='services-blok__section-icon' /></a></button>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+
+
+/* 
+  const skills = [
+    {
+      id: 1,
+      title: 'SMM',
+      body: 'Social Media Marketing, or SMM, is the process of promoting a product or service through social media platforms such as Facebook, Instagram, Twitter, and LinkedIn.',
+      link: 'https://en.wikipedia.org/wiki/Social_media_marketing'
+    },
+    {
+      id: 2,
+      title: 'Frontend Development',
+      body: 'Frontend development is the process of creating the user interface (UI) for web applications or websites. It involves working with HTML, CSS, and JavaScript to build interactive and visually appealing user interfaces.',
+      link: 'https://en.wikipedia.org/wiki/Front-end_web_development'
+    },
+    {
+      id: 3,
+      title: 'Backend Development',
+      body: 'Backend development involves building and maintaining the server-side of web applications or websites. Unlike frontend development, which focuses on the user interface and client-side functionality.',
+      link: 'https://en.wikipedia.org/wiki/Frontend_and_backend'
+    },
+    {
+      id: 4,
+      title: 'Testing and Optimization',
+      body: 'Overall, testing and optimization are essential for delivering high-quality software that meets user expectations and performs well in various environments. By continuously testing and optimizing software throughout the development process.',
+      link: 'https://en.wikipedia.org/wiki/Web_testing'
+    }
+  ]
+
+  return (
+    <div className="services">
+      <div className="services-sar">
+        <h1>SKILLS
+          <div className="sar-animate"></div>
+        </h1>
+      </div>
+      <div className="services-blok">
+        {skills.map(skill => (
+          <Link key={skill.id} to='/skills' className="services-blok__section">
+            <div className="services-blok__icon">
+            </div>
+            <b>{skill.title}</b>
+            <p>{skill.body}</p>
+            <button><a href={skill.link}>Info <IoMdInformationCircle className='services-blok__section-icon' /></a></button>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+*/
+
+
+/* 
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { IoMdInformationCircle } from "react-icons/io";
+
+export default function Skills() {
+
+  const skills = [
+    {
+      id: 1,
+      name: 'SMM',
+      body: 'Social Media Marketing, or SMM, is the process of promoting a product or service through social media platforms such as Facebook, Instagram, Twitter, and LinkedIn.',
+    },
+    {
+      id: 2,
+      name: 'Frontend Development',
+      body: 'Frontend development is the process of creating the user interface (UI) for web applications or websites. It involves working with HTML, CSS, and JavaScript to build interactive and visually appealing user interfaces.',
+    },
+    {
+      id: 3,
+      name: 'Backend Development',
+      body: 'Backend development involves building and maintaining the server-side of web applications or websites. Unlike frontend development, which focuses on the user interface and client-side functionality.',
+    },
+    {
+      id: 4,
+      name: 'Testing and Optimization',
+      body: 'Overall, testing and optimization are essential for delivering high-quality software that meets user expectations and performs well in various environments. By continuously testing and optimizing software throughout the development process.',
+    }
+  ]
+
+  return (
+    <div className="services">
       <div className="services-sar">
         <h1>SKILLS
           <div className="sar-animate"></div>
@@ -22,7 +158,7 @@ export default function Skills() {
         <Link to='/skills' className="services-blok__section">
           <div className="services-blok__icon">
           </div>
-          <b>Frontend Development </b>
+          <b>Frontend Development</b>
           <p>Frontend development is the process of creating the user interface (UI) for web applications or
             websites. It involves working with HTML, CSS, and JavaScript to build interactive and visually
             appealing user interfaces.
@@ -32,7 +168,7 @@ export default function Skills() {
         <Link to='/skills' className="services-blok__section">
           <div className="services-blok__icon">
           </div>
-          <b>Backend Development</b>
+          <b>Backend Development (Strapi)</b>
           <p>Backend development involves building and maintaining the server-side of web applications or
             websites. Unlike frontend development, which focuses on the user interface and client-side
             functionality.</p>
@@ -51,3 +187,4 @@ export default function Skills() {
     </div>
   )
 }
+*/
