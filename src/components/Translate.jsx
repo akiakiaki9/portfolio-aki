f01c7de0c0476d4f4e07220ebe2ea236d98f8924
Akbar <tillayevakbarshoh@gmail.com>
2024-06-12 21:08:52 +0500

    multi-lang 3

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import i18n from '../translates/resources';
 
 export default function Translate() {
