71ef1d992d5a3c4618251e72e7394038dcc1d71e
Akbar <tillayevakbarshoh@gmail.com>
2024-06-21 22:00:05 +0500

    + project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer.jsx'
 import NamePage from '../components/NamePage.jsx'
 import ScrollTop from '../components/ScrollTop.jsx'
 import projectvideo1 from '../images/project1vid.mp4'
+import projectvideo2 from '../images/video2.mp4'
 
 export default function Projects() {
   return (
@@ -21,6 +22,15 @@ export default function Projects() {
                 <div className="project-blok__part">
                   <b>Portfolio Aki</b>
                   <a href="https://akiakiaki9.github.io/portfolio-aki">https://akiakiaki9.github.io/portfolio-aki</a>
+                </div>
+              </div>
+              <div className="project-blok__section">
+                <div className="project-blok__part">
+                  <video src={projectvideo2} typeof='video/mp4' autoPlay muted loop playsInline></video>
+                </div>
+                <div className="project-blok__part">
+                  <b>SYNTAX сайл услуг</b>
+                  <a href="https://syntax-aki.netlify.app">https://syntax-aki.netlify.app</a>
                 </div>
               </div>
             </div>
