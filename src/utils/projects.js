8be6aac684e2b8f90856254353f07f376426939f
Akbar <tillayevakbarshoh@gmail.com>
2024-11-06 09:48:57 +0500

    add a new project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,6 +2,7 @@ import projectvideo1 from '../images/project1vid.mp4'
 import projectvideo2 from '../images/video2.mp4'
 import projectvideo3 from '../images/projectvideo3.mp4'
 import projectvideo4 from '../images/oxford-next.mp4'
+import projectvideo5 from '../images/restaurant.mp4'
 
 const PROJECTS = [
     {
@@ -12,21 +13,27 @@ const PROJECTS = [
     },
     {
         'id': 2,
+        'title': 'Restaurant',
+        'link': 'https://restaurant-ferro-soft.netlify.app',
+        'video': projectvideo5
+    },
+    {
+        'id': 3,
+        'title': 'Oxford IELTS Center (Next)',
+        'link': 'https://oxfordieltscenter-next-aki.netlify.app',
+        'video': projectvideo4
+    },
+    {
+        'id': 4,
         'title': 'SYNTAX сайт услуг',
         'link': 'https://syntax-aki.netlify.app',
         'video': projectvideo2
     },
     {
-        'id': 3,
+        'id': 5,
         'title': 'Oxford IELTS Center (React)',
         'link': 'https://oxford-ielts-center-by-aki.netlify.app',
         'video': projectvideo3
-    },
-    {
-        'id': 4,
-        'title': 'Oxford IELTS Center (Next)',
-        'link': 'https://oxfordieltscenter-next-aki.netlify.app',
-        'video': projectvideo4
     },
 ]
 
